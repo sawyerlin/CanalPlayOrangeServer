025222f29e1d0c12dda94b76a2181838f7576750
sawyer <sawyer.lin@gmail.com>
2014-11-03 18:16:30 +0100

    Remove Origin Header *

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ canalRouter.route('/canal/logs').post(function(req, res) {
 });
 
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	//res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
